eeb976cd433527baa8ead4dde33dd1830bb7f78b
ZavaruKitsu <alexeyzavar@gmail.com>
2024-02-07 23:25:02 +0300

    feat: quick admins & recent actions
    Co-authored-by: c0re100 <corehusky@gmail.com>

--- Telegram/SourceFiles/history/view/history_view_top_bar_widget.h
+++ Telegram/SourceFiles/history/view/history_view_top_bar_widget.h
@@ -231,6 +231,8 @@ private:
 	object_ptr<Ui::IconButton> _search;
 	object_ptr<Ui::IconButton> _infoToggle;
 	object_ptr<Ui::IconButton> _menuToggle;
+	object_ptr<Ui::IconButton> _recentActions;
+	object_ptr<Ui::IconButton> _admins;
 	base::unique_qptr<Ui::PopupMenu> _menu;
 
 	object_ptr<RpWidget> _membersShowArea = { nullptr };
