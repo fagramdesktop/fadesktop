272307c7bdfdb5352fc90f006b95c384147dfe2d
Burhanverse <burhanverse@gmail.com>
2025-11-21 12:59:28 +0530

    `2.0.0` >> `2.0.1`

--- Telegram/SourceFiles/fa/fa_version.h
+++ Telegram/SourceFiles/fa/fa_version.h
@@ -8,7 +8,7 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 
 #pragma once
 
-constexpr auto AppFAVersion = 1000111;
-constexpr auto AppFAVersionStr = "1.1.11";
-constexpr auto AppTGDVersion = "5.15.0";
+constexpr auto AppFAVersion = 2000001;
+constexpr auto AppFAVersionStr = "2.0.1";
+constexpr auto AppTGDVersion = "6.3.2";
 constexpr auto AppFABetaVersion = false;
