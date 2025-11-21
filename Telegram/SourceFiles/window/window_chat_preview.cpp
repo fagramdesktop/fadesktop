16a80a3329b3ffdf36bcc674e3877ff6ec2326c7
Burhanverse <burhanverse@gmail.com>
2025-11-21 07:22:28 +0530

    feat: reduce chat preview delay from 1000ms to 250ms

--- Telegram/SourceFiles/window/window_chat_preview.cpp
+++ Telegram/SourceFiles/window/window_chat_preview.cpp
@@ -21,7 +21,7 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 namespace Window {
 namespace {
 
-constexpr auto kChatPreviewDelay = crl::time(1000);
+constexpr auto kChatPreviewDelay = crl::time(250);
 
 } // namespace
 
