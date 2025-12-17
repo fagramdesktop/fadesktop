1768845c3625c162485b5b85ebabdfc1536a62f5
Burhanverse <contact@burhanverse.eu.org>
2025-12-17 21:10:46 +0530

    fix: use FAgram title on more UI-visible occurrences
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Telegram/SourceFiles/platform/linux/main_window_linux.cpp
+++ Telegram/SourceFiles/platform/linux/main_window_linux.cpp
@@ -228,7 +228,7 @@ void MainWindow::createGlobalMenu() {
 		});
 
 	auto quit = file->addAction(
-		tr::lng_mac_menu_quit_telegram(tr::now, lt_telegram, u"Telegram"_q),
+		tr::lng_mac_menu_quit_telegram(tr::now, lt_telegram, u"FAgram"_q),
 		this,
 		[=] { quitFromTray(); },
 		QKeySequence::Quit);
@@ -428,7 +428,7 @@ void MainWindow::createGlobalMenu() {
 		tr::lng_mac_menu_about_telegram(
 			tr::now,
 			lt_telegram,
-			u"Telegram"_q),
+			u"FAgram"_q),
 		[=] {
 			ensureWindowShown();
 			controller().show(Box(AboutBox));
