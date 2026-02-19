62856c40a21c47e87acaca802f73407c574a6d45
Burhanverse <contact@burhanverse.eu.org>
2026-02-19 15:50:43 +0530

    ci: support grouped upload
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- scripts/tgupload.py
+++ scripts/tgupload.py
@@ -7,6 +7,7 @@
 from os import environ
 import argparse
 from pyrogram import Client, enums
+from pyrogram.types import InputMediaDocument
 # -------------------------------------------------------------------------------
 #                                    VARIABLES
 # -------------------------------------------------------------------------------
@@ -19,7 +20,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN")  # Create a new bot from @BotFather and get
 app = Client("Uploader", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
-async def main(file, chat_id_list, caption):
+async def upload_single(file, chat_id_list, caption):
     async with app:
         for chat_id in chat_id_list:
             await app.send_document(
@@ -31,13 +32,33 @@ async def main(file, chat_id_list, caption):
         print("Upload Successful!")
 
 
+async def upload_group(files, chat_id_list, caption):
+    async with app:
+        media = []
+        for i, file in enumerate(files):
+            media.append(InputMediaDocument(
+                media=file,
+                caption=caption if i == 0 else None,
+                parse_mode=enums.ParseMode.MARKDOWN if i == 0 else None,
+            ))
+        for chat_id in chat_id_list:
+            await app.send_media_group(chat_id=chat_id, media=media)
+        print("Group Upload Successful!")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Uploader", description="Telegram file uploader")
 
-    parser.add_argument("file", type=str, help="File to upload")
+    parser.add_argument("file", type=str, nargs="?", help="File to upload (single file mode)")
+    parser.add_argument("--files", type=str, nargs="+", help="Files to upload as a grouped post")
     parser.add_argument("--chat-id", type=int, help="Chat ID(s) to upload the file to", required=True, nargs="+")
     parser.add_argument("--caption", type=str, help="Caption for the file", required=True)
 
     args = parser.parse_args()
 
-    app.run(main(args.file, args.chat_id, args.caption))
+    if args.files:
+        app.run(upload_group(args.files, args.chat_id, args.caption))
+    elif args.file:
+        app.run(upload_single(args.file, args.chat_id, args.caption))
+    else:
+        parser.error("Either 'file' or '--files' must be provided")
