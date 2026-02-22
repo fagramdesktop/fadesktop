17b10d551a8560214a7b64b63633b82518c36479
Burhanverse <contact@burhanverse.eu.org>
2026-02-22 14:45:20 +0530

    `2.1.7` >> `2.1.8`
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Telegram/SourceFiles/fa/fa_changelogs.cpp
+++ Telegram/SourceFiles/fa/fa_changelogs.cpp
@@ -38,17 +38,6 @@ struct BuiltInChangelog {
 const std::vector<BuiltInChangelog> &GetBuiltInChangelogs() {
 	static const std::vector<BuiltInChangelog> entries = {
 		{
-			2001004,
-			"- Updated tdesktop to v6.4.2\n"
-			"- New redesigned App icon\n"
-			"- Updated corwdin translations\n"
-			"- Added Turkish(tr) translation\n"
-			"- Removed old Beta channel links\n"
-			"- Fixed context menu shortcut icons mode\n\n"
-			"@FAgramDesktop\n"
-
-		},
-		{
 			2001005,
 			"- Added option to auto parse markdown hyperlinks\n"
 			"- Added option to hide sidebar tab titles\n"
@@ -74,6 +63,12 @@ const std::vector<BuiltInChangelog> &GetBuiltInChangelogs() {
 			2001007,
 			"- Updated tdesktop to v6.5.0\n"
 			"- Fixed DM button visibility in channels\n\n"
+			"@FAgramDesktop\n"
+		},
+		{
+			2001008,
+			"- Updated tdesktop to v6.5.1\n"
+			"- Fixed some UI related bugs\n\n"
 			"@FAgramDesktop\n"
 		},
 	};
