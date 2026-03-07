87a43ff47a0905622456f81e823000e4f3698ea5
Burhanverse <contact@burhanverse.eu.org>
2026-03-07 06:31:38 +0530

    ci: update tgupload scripts
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- scripts/tgupload.py
+++ scripts/tgupload.py
@@ -1,24 +1,17 @@
 # -*- coding: utf-8 -*-
 # @author: AbhiTheModder, Burhanverse
 
-# -------------------------------------------------------------------------------
-#                                    IMPORTS
-# -------------------------------------------------------------------------------
-from os import environ
+import asyncio
 import argparse
+from os import environ
+
 from pyrogram import Client, enums
 from pyrogram.types import InputMediaDocument
-# -------------------------------------------------------------------------------
-#                                    VARIABLES
-# -------------------------------------------------------------------------------
+
 API_HASH = environ.get("API_HASH")
-API_ID = int(environ.get("API_ID"))
+API_ID = int(environ.get("API_ID", 0))
 BOT_TOKEN = environ.get("BOT_TOKEN")
 MAX_CAPTION_LENGTH = 1024
-# -------------------------------------------------------------------------------
-#                                    FUNCTIONS
-# -------------------------------------------------------------------------------
-app = Client("Uploader", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
 def truncate_caption(caption):
@@ -27,47 +20,50 @@ def truncate_caption(caption):
     return caption
 
 
-async def upload_single(file, chat_id_list, caption):
-    async with app:
-        for chat_id in chat_id_list:
-            await app.send_document(
-                chat_id=chat_id,
-                document=file,
-                caption=truncate_caption(caption),
-                parse_mode=enums.ParseMode.HTML,
-            )
-        print("Upload Successful!")
+async def upload_single(app, file, chat_id_list, caption):
+    for chat_id in chat_id_list:
+        await app.send_document(
+            chat_id=chat_id,
+            document=file,
+            caption=truncate_caption(caption),
+            parse_mode=enums.ParseMode.HTML,
+        )
+    print("Upload Successful!")
 
 
-async def upload_group(files, chat_id_list, caption):
-    async with app:
-        media = []
-        last_index = len(files) - 1
-        caption = truncate_caption(caption)
-        for i, file in enumerate(files):
-            media.append(InputMediaDocument(
-                media=file,
-                caption=caption if i == last_index else None,
-                parse_mode=enums.ParseMode.HTML if i == last_index else None,
-            ))
-        for chat_id in chat_id_list:
-            await app.send_media_group(chat_id=chat_id, media=media)
-        print("Group Upload Successful!")
+async def upload_group(app, files, chat_id_list, caption):
+    media = []
+    last_index = len(files) - 1
+    caption = truncate_caption(caption)
+    for i, file in enumerate(files):
+        media.append(InputMediaDocument(
+            media=file,
+            caption=caption if i == last_index else None,
+            parse_mode=enums.ParseMode.HTML if i == last_index else None,
+        ))
+    for chat_id in chat_id_list:
+        await app.send_media_group(chat_id=chat_id, media=media)
+    print("Group Upload Successful!")
 
 
-if __name__ == "__main__":
+async def main():
     parser = argparse.ArgumentParser(prog="Uploader", description="Telegram file uploader")
-
     parser.add_argument("file", type=str, nargs="?", help="File to upload (single file mode)")
     parser.add_argument("--files", type=str, nargs="+", help="Files to upload as a grouped post")
     parser.add_argument("--chat-id", type=int, help="Chat ID(s) to upload the file to", required=True, nargs="+")
     parser.add_argument("--caption", type=str, help="Caption for the file", required=True)
-
     args = parser.parse_args()
 
-    if args.files:
-        app.run(upload_group(args.files, args.chat_id, args.caption))
-    elif args.file:
-        app.run(upload_single(args.file, args.chat_id, args.caption))
-    else:
+    if not args.files and not args.file:
         parser.error("Either 'file' or '--files' must be provided")
+
+    app = Client("Uploader", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+    async with app:
+        if args.files:
+            await upload_group(app, args.files, args.chat_id, args.caption)
+        else:
+            await upload_single(app, args.file, args.chat_id, args.caption)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
