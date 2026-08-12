26e5d52ae6d12dbba9d653d8bc351b8dcdabc700
Burhanverse <contact@burhanverse.eu.org>
2026-08-12 19:15:13 +0530

    ci: update action workflows
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Telegram/build/docker/centos_env/build.sh
+++ Telegram/build/docker/centos_env/build.sh
@@ -3,6 +3,6 @@ set -e
 
 cd Telegram
 ./configure.sh "$@"
-cmake --build ../out --config "${CONFIG:-Release}"
+cmake --build ../out --config "${CONFIG:-MinSizeRel}"
 
 ([[ -d ../out/install ]] && rm -rf ../out/install; mkdir -p ../out/install) && DESTDIR=../out/install cmake --install ../out
