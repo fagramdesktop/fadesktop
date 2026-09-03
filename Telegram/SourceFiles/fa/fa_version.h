906545466f03bb66f9656cbebd4ce1dc449ee412
Burhanverse <contact@burhanverse.eu.org>
2026-09-03 16:19:18 +0530

    '2.5.1' -> '2.5.2'
    Signed-off-by: Burhanverse <contact@burhanverse.eu.org>

--- Telegram/SourceFiles/fa/fa_version.h
+++ Telegram/SourceFiles/fa/fa_version.h
@@ -8,7 +8,7 @@ https://github.com/fagramdesktop/fadesktop/blob/dev/LEGAL
 
 #pragma once
 
-constexpr auto AppFAVersion = 2005001;
-constexpr auto AppFAVersionStr = "2.5.1";
+constexpr auto AppFAVersion = 2005002;
+constexpr auto AppFAVersionStr = "2.5.2";
 constexpr auto AppTGDVersion = "7.1.4";
 constexpr auto AppFABetaVersion = false;
